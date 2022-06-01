963f73269cb7f494e5153e9acddae9c7d5afd96b
Osahon Irabor <osahonirabor@hotmail.com>
2022-06-01 16:08:03 -0700

    updated model field

---
+++ files/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class File(models.Model):
+    name = models.CharField(max_length=50)
+    file_type = models.CharField(max_length=10)  
+    upload_timestamp = models.DateTimeField(auto_now_add=True)  
+
+    def __str__(self):
+        return self.name 
--- files/urls.py
+++ files/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #CRUD Create, Read, Update, Delete
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('files/', views.files, name='files'),
